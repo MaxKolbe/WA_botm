6f350ee99dbf92ca5dddf0f6396d8cbb59855815
Max Kolbe <86488799+MaxKolbe@users.noreply.github.com>
2025-08-01 23:31:11 +0100

    Update otpGenerator.js

--- utils/otpGenerator.js
+++ utils/otpGenerator.js
@@ -3,10 +3,11 @@
 
 import { totp, authenticator } from 'otplib'
 
-authenticator.options = { step: 45 } // Code expires every 45 seconds
+authenticator.options = { step: 30 } // Code expires every 30 seconds
 
 // Returns OTP from a secret
 export const generateCode= (secret) => {
     return authenticator.generate(secret)
 }
+
 
