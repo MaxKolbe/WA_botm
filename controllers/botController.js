4e806cd162d282e48eff6c01468eb46e099f40ff
Max Kolbe <86488799+MaxKolbe@users.noreply.github.com>
2025-08-01 18:24:11 +0100

    Update botController.js

--- controllers/botController.js
+++ controllers/botController.js
@@ -53,7 +53,7 @@ Please enter a *phrase* to receive your one-time password (OTP).
 
 You can request up to 3 OTPs. After that, you'll need to wait for a period of time before trying again.
 
-Otps are valid for only 30secs. 
+Otps are valid for only 45secs. 
 
 If you message NenBot and you don't get a reply within a minute (NenBot is NOT disabled or you have NOT reached your usage limits), please resend your message to make sure it goes through.
   </Message>
